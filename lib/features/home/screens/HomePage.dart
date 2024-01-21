8d6616d09e8e062abd90b8ddb072e2895651de7b
Hames-1616 <bilquis748@gmail.com>
2024-01-21 23:35:30 +0530

    📦 NEW: HomeScreen UI building

--- lib/features/home/screens/HomePage.dart
+++ lib/features/home/screens/HomePage.dart
@@ -1,7 +1,9 @@
 import 'package:diabetes_app/core/dimensions.dart';
 import 'package:diabetes_app/core/responsive_text.dart';
+import 'package:diabetes_app/core/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
@@ -15,7 +17,8 @@ class _HomePageState extends ConsumerState<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: MediaQuery.of(context).size.height / hei(context, 200),
+        backgroundColor: primaryColor,
+        toolbarHeight: MediaQuery.of(context).size.height / hei(context, 120),
         actions: [
           Container(
             padding: const EdgeInsets.all(20),
@@ -23,7 +26,8 @@ class _HomePageState extends ConsumerState<HomePage> {
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Row(
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     ResponsiveText(
                         text: "Hi Haamid!",
@@ -33,12 +37,12 @@ class _HomePageState extends ConsumerState<HomePage> {
                           fontSize: 28,
                         )),
                     ResponsiveText(
-                        text: "${DateFormat('EEEE').format(DateTime.now())}",
+                        text:
+                            "${DateFormat('EEEE').format(DateTime.now())}, ${DateFormat('dd').format(DateTime.now())} ${DateFormat('MMMM').format(DateTime.now())}",
                         style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontFamily: "poppins",
-                          fontSize: 28,
-                        )),
+                            fontFamily: "poppins",
+                            fontSize: 16,
+                            color: Colors.white60)),
                   ],
                 ),
                 IconButton(
@@ -52,9 +56,37 @@ class _HomePageState extends ConsumerState<HomePage> {
           )
         ],
       ),
-      body: Center(
-        child: Text("data"),
-      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height/hei(context, 180),
+              margin: const EdgeInsets.only(left: 20,right: 20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: secondaryColor
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(left: 15),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        ResponsiveText(
+                            text: "Eaten",
+                            style: const TextStyle(fontFamily: "poppins",fontSize: 20,fontWeight: FontWeight.bold),
+                          ),
+                        
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      )
     );
   }
 }
