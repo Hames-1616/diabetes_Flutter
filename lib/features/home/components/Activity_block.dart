85c60ae40f50ce994707d63d17e944292e5d3d5b
Hames-1616 <bilquis748@gmail.com>
2024-01-24 21:40:51 +0530

    👌 IMPROVE: added content to the activity blocks

--- lib/features/home/components/Activity_block.dart
+++ lib/features/home/components/Activity_block.dart
@@ -1,4 +1,5 @@
 import 'package:diabetes_app/core/dimensions.dart';
+import 'package:diabetes_app/core/responsive_text.dart';
 import 'package:diabetes_app/core/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -14,12 +15,52 @@ class _ActivityBlockState extends ConsumerState<ActivityBlock> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height / hei(context, 80),
-      width: 170,
-      decoration: BoxDecoration(
-        color: secondaryColor,
-        borderRadius: BorderRadius.circular(20)
+    return InkWell(
+      onTap: (){},
+      child: Container(
+        height: MediaQuery.of(context).size.height / hei(context, 80),
+        width: 170,
+        decoration: BoxDecoration(
+          color: secondaryColor,
+          borderRadius: BorderRadius.circular(20)
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ResponsiveText(
+                  text: "Glucose",
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 15.5,
+                    fontFamily: "poppins"),
+                ),
+      
+                Row(
+                  children: [
+                    ResponsiveText(
+                      text: "136 ",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 13,
+                        fontFamily: "poppins"),
+                    ),
+                    ResponsiveText(
+                      text: "mg/dl ",
+                      style: const TextStyle(
+                        color: Colors.white60,
+                        fontSize: 13,
+                        fontFamily: "poppins"),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            Container()
+          ],
+        ),
       ),
     );
   }
