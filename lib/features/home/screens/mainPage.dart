a0d512854152e4a0b0ddb5d829bf4fb0206fc25d
Hames-1616 <bilquis748@gmail.com>
2024-02-01 23:00:58 +0530

    🐛 FIX: navbar is not working fine

--- lib/features/home/screens/mainPage.dart
+++ lib/features/home/screens/mainPage.dart
@@ -1,34 +1,60 @@
 import 'package:diabetes_app/core/themes.dart';
+import 'package:diabetes_app/features/home/screens/HomePage.dart';
 import 'package:dot_navigation_bar/dot_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class Home extends ConsumerWidget {
+class Home extends ConsumerStatefulWidget {
   const Home({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    int selected = 0;
+  ConsumerState<ConsumerStatefulWidget> createState() => _HomeState();
+}
+int _currentIndex = 0;
+class _HomeState extends ConsumerState<Home> {
+  void changePage(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+
+  final pages = [const HomePage(), Container(), Container(), Container()];
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.transparent,
-        ),
-        body: Container(),
-        bottomNavigationBar: Container(
-          margin: const EdgeInsets.only(bottom: 10),
-          child: DotNavigationBar(
-            
-            itemPadding: EdgeInsets.all(5),
-              currentIndex: selected,
-              onTap: (p0) {
-                selected = p0;
-              },
-              selectedItemColor: primaryColor,
-              items: [
-                DotNavigationBarItem(icon: const Icon(Icons.home)),
-                DotNavigationBarItem(icon: const Icon(Icons.settings))
-              ]),
-        ));
+      extendBody: true,
+      body: pages[_currentIndex],
+      bottomNavigationBar: DotNavigationBar(
+        backgroundColor: Colors.white70,
+        selectedItemColor: primaryColor,
+        unselectedItemColor: Colors.black45,
+        marginR: EdgeInsets.only(left: 20,right: 20),
+        paddingR: EdgeInsets.only(top: 5,bottom: 5,right: 10,left: 10),
+        currentIndex: _currentIndex,
+        onTap: changePage,
+        dotIndicatorColor: Colors.transparent,
+        items: [
+          /// Home
+          DotNavigationBarItem(
+              icon: const Icon(Icons.home),
+          ),
+
+          /// Likes
+          DotNavigationBarItem(
+              icon: const Icon(Icons.favorite_border),
+              ),
+
+          /// Search
+          DotNavigationBarItem(
+              icon: const Icon(Icons.search),
+              ),
+
+          /// Profile
+          DotNavigationBarItem(
+              icon: const Icon(Icons.person),
+           ),
+        ],
+      ),
+    );
   }
 }
